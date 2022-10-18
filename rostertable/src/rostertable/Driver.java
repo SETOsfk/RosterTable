f9f8382ec2a5c8930d66df713aa553217f6128f8
Sertan <sertansafak@posta.mu.edu.tr>
2022-10-18 16:35:12 +0300

    RosterTable

---
+++ rostertable/src/rostertable/Driver.java
@@ -0,0 +1,37 @@
+package rostertable;
+import java.util.ArrayList;
+public class Driver {
+
+	public static void main(String[] args) {
+		int days =3;
+		//array list of arraylists
+		ArrayList<ArrayList<String>> roster =
+				new ArrayList<ArrayList<String>>(days);
+		//each sub-arraylist
+		ArrayList<String>day1 = new ArrayList<String>();
+		ArrayList<String>day2 = new ArrayList<String>();
+		ArrayList<String>day3 = new ArrayList<String>();
+		day1.add("Alice");
+		day1.add("Bob");
+		roster.add(day1);
+		day2.add("Alice");
+		day2.add("Sam");
+		day2.add("Mike");
+		roster.add(day2);
+		day3.add("Ray");
+		day3.add("Sam");
+		roster.add(day3);
+		getDays(roster, "Alice");
+		getDays(roster, "Sam");
+		
+
+	}
+
+	private static void getDays(ArrayList<ArrayList<String>> roster, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+		
+	
+}
